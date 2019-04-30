c8ac8f3c1bf2fe4059e2393ea909c9824f940037
Joel W. King <joel.king@wwt.com>
2019-04-30 16:46:12 -0400

    style updates and flake8 clean-up

--- xls_to_csv.py
+++ xls_to_csv.py
@@ -64,60 +64,63 @@ xl.write_csv(result,'/tmp/')
 ERROR = 0
 OK = 1
 EXTENSION = '.csv'
+START_WITH_LETTER = 'Z_'
+FACTS = 'ansible_facts'
 
-import os
+#
+# Imports
+#
 import csv
 import re
 
 from ansible.module_utils.basic import AnsibleModule
 
 try:
-    MISSING_LIB = False
+    HAS_LIB = True
     import pandas as pd
-    import xlrd                                            # pandas Install xlrd >= 0.9.0 for Excel support
+    # import xlrd                                           # pandas Install xlrd >= 0.9.0 for Excel support
 except ImportError:
-    MISSING_LIB = True    
+    HAS_LIB = False
 
 
 def read_xls(input_file, sheets):
     """
     Read the .xlsx file and load everything into a dictionary of facts
     """
-    result = {"ansible_facts": dict(),
+    result = {FACTS: dict(),
               "warnings": []}
     data_frame = dict()                                    # each sheet in the file stored as data frame
-    
+
     try:
         xlsx = pd.ExcelFile(input_file)
     except IOError:
         return (ERROR, "IOError on input file:%s" % input_file)
 
-    
     for sheet in xlsx.sheet_names:                         # list of sheet names
         if sheet in sheets:
-            data_frame[sheet] = xlsx.parse(sheet)              # read a specific sheet to DataFrame 
-            result["ansible_facts"][get_valid_name(sheet)] = get_rows(data_frame[sheet])
+            data_frame[sheet] = xlsx.parse(sheet)              # read a specific sheet to DataFrame
+            result[FACTS][get_valid_name(sheet)] = get_rows(data_frame[sheet])
         else:
             result['warnings'].append(' skipping sheet {} in source file'.format(sheet))
-    
+
     return (OK, result)
 
 
 def get_valid_name(name):
     """
         per Ansible https://docs.ansible.com/ansible/latest/user_guide/playbooks_variables.html
-        Variable names should be letters, numbers, and underscores. 
+        Variable names should be letters, numbers, and underscores.
         Variables should always start with a letter.
 
         Remove special characters, spaces, leave underscores
 
     """
-    name = re.sub(r"[^a-zA-Z0-9_]","", name)
+    name = re.sub(r"[^a-zA-Z0-9_]", "", name)
 
     if name[0].isalpha():
         return name
-    
-    return 'Z_{}'.format(name)
+
+    return '{}{}'.format(START_WITH_LETTER, name)
 
 
 def get_rows(data_frame):
@@ -136,8 +139,9 @@ def get_rows(data_frame):
             row[column_name] = row_content[item]
             # row[data_frame.columns[item]] = row_content[item]
         rows.append(row)
-    
+
     return rows                                            # rows is a list of dictionaries
+
 
 def write_csv(result, destination_dir):
     """
@@ -149,26 +153,26 @@ def write_csv(result, destination_dir):
     if not destination_dir.endswith('/'):
         destination_dir + '/'
 
-    for sheet in result['ansible_facts'].keys():
+    for sheet in result[FACTS].keys():
         try:
             output_file = destination_dir + sheet + EXTENSION
             f = open(output_file, 'w')
         except IOError:
             return (ERROR, "IOError on output file:%s" % output_file)
 
-        fieldnames = result['ansible_facts'][sheet][0].keys() # TODO MERGE WITH BELOW
+        fieldnames = result[FACTS][sheet][0].keys()  # TODO MERGE WITH BELOW
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
-        
-        for row in result['ansible_facts'][sheet]:
+
+        for row in result[FACTS][sheet]:
             try:
                 writer.writerow(row)
             except UnicodeEncodeError:
                 return(ERROR, '{} UnicodeEncodeError: {}'.format(sheet, row))
-            
+
         f.close()
 
-    result['ansbile_facts'] = {'sheet_filenames': result['ansible_facts'].keys()}
+    result[FACTS] = {'sheet_filenames': result[FACTS].keys()}
 
     return (OK, result)
 
@@ -177,33 +181,32 @@ def main():
     """
         Validate args, check for non-standard libraries, call functions to read XLS and write CSV
     """
-    module = AnsibleModule(argument_spec = dict(
-                 src = dict(required=True),
-                 dest = dict(required=True),
-                 sheets=dict(required=True, type='list')
+    module = AnsibleModule(argument_spec=dict(
+                src=dict(required=True),
+                dest=dict(required=True),
+                sheets=dict(required=True, type='list')
              ),
              add_file_common_args=True)
 
-    # we use the filename plus the sheet name
-    # filename, extension = os.path.splitext(os.path.basename(input_file))
-
-    if MISSING_LIB:
+    # Check if we have the necessary libraries, fail gracefully if not
+    if not HAS_LIB:
         module.fail_json(msg="The pandas and xlrd Python modules are required, install using: sudo pip install ...")
 
+    # Read the Excel file
     status, result = read_xls(module.params["src"], module.params["sheets"])
-
     if status == ERROR:
         module.fail_json(msg=result)
-    else:                                                  # Write sheets to destination directory
-        status, result = write_csv(result, module.params.get("dest"))
-        if status == ERROR:
-            module.fail_json(msg=result)
 
-        module.exit_json(**result)
+    # Write sheets to destination directory
+    status, result = write_csv(result, module.params.get("dest"))
+    if status == ERROR:
+        module.fail_json(msg=result)
 
-    return code
+    module.exit_json(**result)
+
 
 if __name__ == '__main__':
-    """ Run the program
+    """
+        Run the program
     """
     main()
